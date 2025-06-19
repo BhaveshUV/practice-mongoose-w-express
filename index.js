bebc7804f1494c4fa1f31858b4b3e785b720ec7a
Bhavesh Vishwakarma <bhaveshruv@gamil.com>
2025-06-19 14:58:01 +0530

    create Chat model, its instance and save to db
    - create Chat model in models folder
    - import it in index.js file and create an instance of Chat
    - save it to db

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const Chat = require("./models/chat");
 
+//---------------- Set up MongoDB database connection ----------------//
 async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/chatApp');
 }
@@ -10,6 +12,18 @@ main()
     .then(() => console.log("Connected to MongoDB server using mongoose"))
     .catch(err => console.log(err));
 
+//-------- Create a chat instance and save to chatApp database --------//
+let chat1 = new Chat({
+    from: "agentA",
+    to: "agentB",
+    msg: "Work-in progress",
+    created_at: new Date()
+});
+chat1.save()
+    .then(res => console.log(res))
+    .catch(e => console.log(e));
+
+//---------------- Set up Express server and API routes ----------------//
 const app = express();
 
 app.set("view engine", "ejs");

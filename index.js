18e1ae018399bb4a5af84e5c39fab01d66014f1d
Bhavesh Vishwakarma <bhaveshruv@gamil.com>
2025-06-20 16:16:55 +0530

    add post newChat endpt, create pg to send new chat
    - add get endpoint to get form to send new message
    - update style.css in /public for 'send new message' form
    - add the new message to db and redirect to 'view all chats' pg
      (index.ejs)

--- index.js
+++ index.js
@@ -19,14 +19,38 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
 app.use(express.static(path.join(__dirname, "/public")));
+// Middleware
+app.use(express.urlencoded({ extended: true }));
 
+// GET—check root get route
 app.get("/", (req, res) => {
     res.send("This root GET API is working");
 });
 
+// GET — get all chats
 app.get("/chats", async (req, res) => {
     let chats = await Chat.find();
-    res.render("index.ejs", {chats});
+    res.render("index.ejs", { chats });
+});
+
+// GET — get send new chat form
+app.get("/chats/new", (req, res) => {
+    res.render("newChatForm.ejs");
+})
+
+// POST — create/insert new received chat
+app.post("/chats", (req, res) => {
+    console.log("request received");
+    let newChat = new Chat({
+        ...req.body,
+        created_at: new Date()
+    })
+    newChat.save()
+        .then(result => {
+            console.log("The message is saved to db", result);
+            res.redirect("/chats");
+        })
+        .catch(e => res.send(e));
 });
 
 app.listen(8080, () => {
